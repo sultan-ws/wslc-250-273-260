7f7d5ff732f08d28fd9ab2f040307a6dc48300e2
sultanBytes <sultankhand11@gmail.com>
2024-09-04 21:07:53 +0530

    update and delete api

--- backend/009 file uploadation/index.js
+++ backend/009 file uploadation/index.js
@@ -9,7 +9,7 @@ const dbname = 'temp_users_data';
 
 const client = new mongodb.MongoClient(url);
 
-const config = async()=>{
+const config = async () => {
     await client.connect();
 
     const db = client.db(dbname);
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use('/api-services', express.static('uploads'));
 
 const multerStorage = multer.diskStorage({
-    destination:(req, file, cb)=>{
+    destination: (req, file, cb) => {
         cb(null, 'uploads');
     },
-    filename:(req, file, cb)=>{
+    filename: (req, file, cb) => {
         // console.log(file);
         const ext = path.extname(file.originalname)
         cb(null, Date.now() + Math.floor(Math.random() * 99999) + ext);
@@ -43,37 +43,37 @@ const multerStorage = multer.diskStorage({
 
 
 //*** multiple fields */
-const upload = multer({storage:multerStorage}).fields(
+const upload = multer({ storage: multerStorage }).fields(
     [
         { name: 'images', maxCount: 10 },
         { name: 'thumbnail', maxCount: 1 },
-        
+
     ]
 );
 
-app.post('/insert-data',upload, async(req,res)=>{
+app.post('/insert-data', upload, async (req, res) => {
 
     const db = await config();
     const users = db.collection('users');
 
-    try{
+    try {
         const data = req.body;
         const files = req.files;
 
         // console.log(req.file);  if single file to be uploaded
         //console.log(req.files);  //multiple files to be uploaded
 
-        if(req.files){
+        if (req.files) {
             data.thumbnail = req.files.thumbnail[0].filename;
 
-            data.images = req.files.images.map((image)=>(image.filename));
+            data.images = req.files.images.map((image) => (image.filename));
         }
 
 
 
         const response = await users.insertOne(data);
 
-        res.status(200).json({message: 'data added successfully', data: response});
+        res.status(200).json({ message: 'data added successfully', data: response });
 
         // if(req.file){
 
@@ -86,50 +86,133 @@ app.post('/insert-data',upload, async(req,res)=>{
         //         fs.unlinkSync(path.join(__dirname, `uploads/${req.file.filename}`));
         //         return;
         //     }
-            
+
         // }
 
-        
+
     }
-    catch(error){
+    catch (error) {
         console.log(error);
-        res.status(500).json({message: 'internal server error'});
+        res.status(500).json({ message: 'internal server error' });
     }
 });
 
-app.get('/read-users', async(req, res)=>{
+app.get('/read-users', async (req, res) => {
     const db = await config();
     const users = db.collection('users');
 
     const filePath = `${req.protocol}://${req.get('host')}/api-services`;
 
-    try{
+    try {
         const response = await users.find().toArray();
 
-        res.status(200).json({message: 'data fetched successfully', data: response, file_path:filePath });
+        res.status(200).json({ message: 'data fetched successfully', data: response, file_path: filePath });
     }
-    catch(error){
+    catch (error) {
         console.log(error);
-        res.status(500).json({messsage:'internal server error'});
+        res.status(500).json({ messsage: 'internal server error' });
     }
 });
 
-app.get('/read-user-by-id/:id', async(req,res)=>{
+app.get('/read-user-by-id/:id', async (req, res) => {
     const db = await config();
     const users = db.collection('users');
 
-    try{
-        const response = await users.find({_id: new mongodb.ObjectId(req.params.id)}).toArray();
-        res.status(200).json({message: 'data fetched successfully', data:response });
+    try {
+        const response = await users.find({ _id: new mongodb.ObjectId(req.params.id) }).toArray();
+        res.status(200).json({ message: 'data fetched successfully', data: response });
     }
-    catch(error){
+    catch (error) {
         console.log(error);
-        res.status(500).json({message: 'internal server error'});
+        res.status(500).json({ message: 'internal server error' });
+    }
+});
+
+app.put('/update-user/:id', upload, async (req, res) => {
+    const db = await config();
+    const users = db.collection('users');
+    try {
+        const newData = req.body;
+
+        const preData = await users.find({ _id: new mongodb.ObjectId(req.params.id) }).toArray();
+
+
+        if (req.files.thumbnail) {
+            newData.thumbnail = req.files.thumbnail[0].filename;
+
+            if (preData[0].thumbnail) {
+                fs.unlinkSync(path.join(__dirname, `uploads/${preData[0].thumbnail}`));
+            }
+        }
+
+        if (req.files.images) {
+            newData.images = req.files.images.map((image) => (image.filename));
+
+            if (preData[0].images) {
+
+                preData[0].images.forEach((img) => {
+                    if (fs.existsSync(path.join(__dirname, `uploads/${img}`))) {
+                        fs.unlinkSync(path.join(__dirname, `uploads/${img}`));
+                    }
+                });
+
+            }
+        }
+
+        // console.log(newData);
+
+        const response = await users.updateOne(
+            {
+                _id: new mongodb.ObjectId(req.params.id)
+            },
+            {
+                $set: newData
+            }
+        );
+
+        res.status(200).json({ message: 'success', data: 'response' });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'internal server error' });
     }
 });
 
 
-app.listen(5200, ()=>{
+app.delete('/delete-user/:id', async (req, res) => {
+    const db = await config();
+    const users = db.collection('users');
+
+    try {
+        const preData = await users.findOne({ _id: new mongodb.ObjectId(req.params.id) });
+
+        console.log(preData);
+
+        if (preData.thumbnail) {
+            fs.unlinkSync(path.join(__dirname, `uploads/${preData.thumbnail}`));
+        }
+
+        if (preData.images) {
+
+
+            preData.images.forEach((img) => {
+                if (fs.existsSync(path.join(__dirname, `uploads/${img}`))) {
+                    fs.unlinkSync(path.join(__dirname, `uploads/${img}`));
+                }
+            });
+
+        }
+
+        const response = await users.deleteOne({_id : new mongodb.ObjectId( req.params.id )});
+        res.status(200).json({ message: 'success', data: response });
+}
+    catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'internal server error' });
+}
+});
+
+app.listen(5200, () => {
     console.log('server is running on port 5200');
 });
 
